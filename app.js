0731b1f2896a8fe2433a7d597a7feb782c291da8
Tanudee <110951920+Tanudee@users.noreply.github.com>
2023-08-09 00:49:54 +1000

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+var glossaryRouter = require('./routes/glossary');
+app.use('/glossary', glossaryRouter);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+module.exports = app;
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
